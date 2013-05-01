0e88335a71d73ed38756642d798e37d0eb6082d1
MaartenDEV <maarten.developer@gmail.com>
2013-05-01 17:10:51 +0200

    add readme

---
+++ README.mk
@@ -0,0 +1,7 @@
+====================================
+       Shadow Store  AOSP
+====================================
+
+
+This is the AOSP version of the Shadow Store included in the ShadowDX ROM for the HTC Desire X.
+This is a version without any type of HTC Sense in it.
